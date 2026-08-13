b0d0dcfb3bf7cd032471b973e2e1ab1a4b8c75a0
skarmik <armandomca2111@agmail.com>
2026-08-13 09:08:17 -0600

    feat: setup initial NestJS configuration with Prisma and MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FoodsModule } from './foods/foods.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
+import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
   imports: [],
